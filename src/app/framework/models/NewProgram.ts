79de4844c6664b27275783faf23ea2333f27f9fc
Shravanhvs123 <shravanahvs@gmail.com>
2022-09-22 16:36:15 +0530

    Intial Commit

---
+++ src/app/framework/models/NewProgram.ts
@@ -0,0 +1,9 @@
+export class NewProgram {
+  public id: number;
+  public name: string;
+  public description: strings;
+  public admin: number;
+  public users: number[] = [];
+  constructor() {
+  }
+}
