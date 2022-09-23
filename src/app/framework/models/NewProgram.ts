3e280086cb635bff8fdacdf97d93899a88bf1dd7
Shravanhvs123 <shravanahvs@gmail.com>
2022-09-23 12:11:34 +0530

    Cmmit 10

--- src/app/framework/models/NewProgram.ts
+++ src/app/framework/models/NewProgram.ts
@@ -1,6 +1,6 @@
 export class NewProgram {
   public id: number;
-  public name: strings;
+  public name: string;
   public description: string;
   public admin: number;
   public users: number[] = [];
